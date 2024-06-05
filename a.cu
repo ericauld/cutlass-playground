23b2bec3001936ebdafd799265f97d66ce4b60bb
Eric Auld <ericauld@gmail.com>
2024-06-05 18:47:42 +0000

    a has printing problems and static assert fails

--- a.cu
+++ a.cu
@@ -1,12 +1,10 @@
-#include <thrust/host_vector.h>
-#include <thrust/device_vector.h>
-#include <cute/tensor.hpp>
-
-#include "cutlass/util/print_error.hpp"
-
 #include <iostream>
 #include <cassert>
 #include <cmath>
+
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <cute/tensor.hpp>
 
 // Simple CPU implementation of matrix multiplication, assuming A is transposed
 void matrix_multiply_cpu(const cute::half_t* A, const cute::half_t* B, cute::half_t* C, int m, int n, int k) {
@@ -15,9 +13,9 @@ void matrix_multiply_cpu(const cute::half_t* A, const cute::half_t* B, cute::hal
       cute::half_t sum = static_cast<cute::half_t>(0.0f);
       for (int p = 0; p < k; ++p) {
         // A is transposed, access it as A[p + i*k]
-        sum += A[p + i * k] * B[p + j * k];
+        sum += A[i * k + p] * B[p + j * k];
       }
-      C[i * n + j] = sum;
+      C[i + m * j] = sum;
     }
   }
 }
@@ -43,7 +41,7 @@ f(cute::half_t const *A,
   using namespace cute;
 
   Tensor mA = make_tensor(make_gmem_ptr(A), make_layout(make_shape(_16{}, _16{}), make_stride(_16{}, _1{})));
-  Tensor mB = make_tensor(make_gmem_ptr(B), make_shape(_8{}, _16{}));
+  Tensor mB = make_tensor(make_gmem_ptr(B), make_shape(_16{}, _8{}));
   Tensor mC = make_tensor(make_gmem_ptr(C), make_shape(_16{}, _8{})); 
   auto thrmma = my_mma.get_slice(threadIdx.x);
 
@@ -55,7 +53,7 @@ f(cute::half_t const *A,
   auto tCmA = thrmma.partition_A(mA);
   auto tCmB = thrmma.partition_B(mB);
 
-#if 0
+#if 1
   print("mA : "); print(mA); print("\n");
   print("mB : "); print(mB); print("\n");
   print("mC : "); print(mC); print("\n");
@@ -66,7 +64,7 @@ f(cute::half_t const *A,
   print("tCmB : "); print(tCmB); print("\n");
   print("tCmC : "); print(tCmC); print("\n");
 #endif
-#if 1
+#if 0
   copy(tCmA, rA);
   copy(tCmB, rB);
   gemm(my_mma, rA, rB, rC);
@@ -108,23 +106,26 @@ int main() {
   thrust::device_vector<TA> d_B = h_B;
   thrust::device_vector<TA> d_C = h_C;
 
-  matrix_multiply_cpu(h_A.data(), h_B.data(), h_C_ref.data(), m, n, k);
-
   using op = SM80_16x8x16_F16F16F16F16_TN;
   auto tiled_mma = make_tiled_mma(op{}, make_layout(make_shape(_1{}, _1{}, _1{}))); 
 
   dim3 dimGrid(1);
   dim3 dimBlock(32);
+  
   f<<<dimGrid, dimBlock>>>(d_A.data().get(), d_B.data().get(), d_C.data().get(), tiled_mma);
+  matrix_multiply_cpu(h_A.data(), h_B.data(), h_C_ref.data(), m, n, k);
+
   thrust::copy(d_C.begin(), d_C.end(), h_C.begin());
-#if 1
+#if 0
   print("h_A : "); printMatrix(h_A.data(), m, k); print("\n\n");
   print("h_B : "); printMatrix(h_B.data(), k, n); print("\n\n");
   print("h_C : "); printMatrix(h_C.data(), m, n); print("\n\n");
   print("h_C_ref : "); printMatrix(h_C_ref.data(), m, n); print("\n\n");
 #endif
+# if 0
   assert(areMatricesEqual(h_C.data(), h_C_ref.data(), m, n));
 
   std::cout << "Success!" << std::endl;
+#endif
   return 0;
 }
