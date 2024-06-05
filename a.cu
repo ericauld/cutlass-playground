1aad0782380546fc3e47ea8cfbf247955387d1c0
Eric Auld <ericauld@gmail.com>
2024-06-05 17:30:00 +0000

    Fix compile debug, a.cu doesn't work and I don't know why

--- a.cu
+++ a.cu
@@ -42,19 +42,20 @@ f(cute::half_t const *A,
   TiledMma            my_mma) {
   using namespace cute;
 
-  Tensor mA = make_tensor(make_gmem_ptr(A), make_layout(make_shape(_16{}, _16{}), make_stride(_1{}, _16{})));
+  Tensor mA = make_tensor(make_gmem_ptr(A), make_layout(make_shape(_16{}, _16{}), make_stride(_16{}, _1{})));
   Tensor mB = make_tensor(make_gmem_ptr(B), make_shape(_8{}, _16{}));
   Tensor mC = make_tensor(make_gmem_ptr(C), make_shape(_16{}, _8{})); 
   auto thrmma = my_mma.get_slice(threadIdx.x);
 
   auto rC = thrmma.partition_fragment_C(mC);
+  clear(rC);
   auto rA = thrmma.partition_fragment_A(mA);
   auto rB = thrmma.partition_fragment_B(mB);
   auto tCmC = thrmma.partition_C(mC);
   auto tCmA = thrmma.partition_A(mA);
   auto tCmB = thrmma.partition_B(mB);
 
-#if 1
+#if 0
   print("mA : "); print(mA); print("\n");
   print("mB : "); print(mB); print("\n");
   print("mC : "); print(mC); print("\n");
@@ -65,11 +66,24 @@ f(cute::half_t const *A,
   print("tCmB : "); print(tCmB); print("\n");
   print("tCmC : "); print(tCmC); print("\n");
 #endif
-#if 0
+#if 1
+  copy(tCmA, rA);
+  copy(tCmB, rB);
   gemm(my_mma, rA, rB, rC);
   copy(rC, tCmC);
 #endif
   return;
+}
+
+void printMatrix(const cute::half_t* data, int rows, int cols) {
+    std::cout << std::fixed << std::setprecision(4);
+    for (int i = 0; i < rows; ++i) {
+        std::cout << "[ ";
+        for (int j = 0; j < cols; ++j) {
+            std::cout << static_cast<float>(data[i * cols + j]) << " ";
+        }
+        std::cout << "]" << std::endl;
+    }
 }
 
 int main() {
@@ -103,9 +117,14 @@ int main() {
   dim3 dimBlock(32);
   f<<<dimGrid, dimBlock>>>(d_A.data().get(), d_B.data().get(), d_C.data().get(), tiled_mma);
   thrust::copy(d_C.begin(), d_C.end(), h_C.begin());
+#if 1
+  print("h_A : "); printMatrix(h_A.data(), m, k); print("\n\n");
+  print("h_B : "); printMatrix(h_B.data(), k, n); print("\n\n");
+  print("h_C : "); printMatrix(h_C.data(), m, n); print("\n\n");
+  print("h_C_ref : "); printMatrix(h_C_ref.data(), m, n); print("\n\n");
+#endif
   assert(areMatricesEqual(h_C.data(), h_C_ref.data(), m, n));
 
   std::cout << "Success!" << std::endl;
   return 0;
 }
-
