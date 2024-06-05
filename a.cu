2a868b87bf85c84dbaf948f08910bd898363ec78
Eric Auld <ericauld@gmail.com>
2024-06-05 00:11:35 +0000

    a.cu works even though it shouldn't

--- a.cu
+++ a.cu
@@ -2,6 +2,35 @@
 #include <thrust/device_vector.h>
 #include <cute/tensor.hpp>
 
+#include <iostream>
+#include <cassert>
+#include <cmath>
+
+// Simple CPU implementation of matrix multiplication, assuming A is transposed
+void matrix_multiply_cpu(const cute::half_t* A, const cute::half_t* B, cute::half_t* C, int m, int n, int k) {
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < n; ++j) {
+      cute::half_t sum = static_cast<cute::half_t>(0.0f);
+      for (int p = 0; p < k; ++p) {
+        // A is transposed, access it as A[p + i*k]
+        sum += A[p + i * k] * B[p + j * k];
+      }
+      C[i * n + j] = sum;
+    }
+  }
+}
+
+bool areMatricesEqual(const cute::half_t* C1, const cute::half_t* C2, int m, int n, float tolerance = 1e-3) {
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < n; ++j) {
+      if (std::fabs(static_cast<float>(C1[i * n + j]) - static_cast<float>(C2[i * n + j])) > tolerance) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
 template <class TiledMma>
 __global__ static
 void
@@ -31,23 +60,26 @@ f(cute::half_t const *A,
 int main() {
   using namespace cute;
 
-  int M = 16;
-  int N = 8;
-  int K = 16;
+  int m = 16;
+  int n = 8;
+  int k = 16;
 
   using TA = half_t;
 
-  thrust::host_vector<TA> h_A(M*K);
-  thrust::host_vector<TA> h_B(K*N);
-  thrust::host_vector<TA> h_C(M*N);
+  thrust::host_vector<TA> h_A(m*k);
+  thrust::host_vector<TA> h_B(k*n);
+  thrust::host_vector<TA> h_C(m*n);
+  thrust::host_vector<TA> h_C_ref(m*n);
 
-  for (int j = 0; j < M*K; ++j) h_A[j] = static_cast<TA>( 2*(rand() / double(RAND_MAX)) - 1 );
-  for (int j = 0; j < N*K; ++j) h_B[j] = static_cast<TA>( 2*(rand() / double(RAND_MAX)) - 1 );
-  for (int j = 0; j < M*N; ++j) h_C[j] = 0;
+  for (int j = 0; j < m*k; ++j) h_A[j] = static_cast<TA>( 2*(rand() / double(RAND_MAX)) - 1 );
+  for (int j = 0; j < n*k; ++j) h_B[j] = static_cast<TA>( 2*(rand() / double(RAND_MAX)) - 1 );
+  for (int j = 0; j < m*n; ++j) h_C[j] = 0;
 
   thrust::device_vector<TA> d_A = h_A;
   thrust::device_vector<TA> d_B = h_B;
   thrust::device_vector<TA> d_C = h_C;
+
+  matrix_multiply_cpu(h_A.data(), h_B.data(), h_C_ref.data(), m, n, k);
 
   using op = SM80_16x8x16_F16F16F16F16_TN;
   auto tiled_mma = make_tiled_mma(op{}, make_layout(make_shape(_1{}, _1{}, _1{}))); 
@@ -55,5 +87,9 @@ int main() {
   dim3 dimGrid(1);
   dim3 dimBlock(32);
   f<<<dimGrid, dimBlock>>>(d_A.data().get(), d_B.data().get(), d_C.data().get(), tiled_mma);
+  thrust::copy(d_C.begin(), d_C.end(), h_C.begin());
+  assert(areMatricesEqual(h_C.data(), h_C_ref.data(), m, n));
+
+  std::cout << "Success" << std::endl;
   return 0;
 }
