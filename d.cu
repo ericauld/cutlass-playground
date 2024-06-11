49109706230d70342d2eb203a10a4ca6673e3302
Eric Auld <ericauld@gmail.com>
2024-06-11 04:06:42 +0000

    d.cu still not working, add print output in comments

--- d.cu
+++ d.cu
@@ -58,40 +58,13 @@ f(cute::half_t const *A,
   Tensor tCgB = thrmma.partition_B(gB);
   Tensor tCmC = thrmma.partition_C(mC);
 
-  auto rC = thrmma.partition_fragment_C(mC);
+  auto rC = thrmma.partition_fragment_C(tCmC);
   clear(rC);
-#if 0
-  if (thread0()) {
-    print("mA : "); print(mA); print("\n");
-    print("mB : "); print(mB); print("\n");
-    print("mC : "); print(mC); print("\n");
-    print("gA : "); print(gA); print("\n");
-    print("gB : "); print(gB); print("\n");
-    print("rC : "); print(rC); print("\n");
-    print("tCgA : "); print(tCgA); print("\n");
-    print("tCgB : "); print(tCgB); print("\n");
-    print("tCmC : "); print(tCmC); print("\n");
-  }
-#endif
-/*
-mA : gmem_ptr[16b](0x7f3f23c00000) o (_16,112):(112,_1)
-mB : gmem_ptr[16b](0x7f3f23c00e00) o (_8,112):(112,_1)
-mC : gmem_ptr[16b](0x7f3f23c01600) o (_16,_8):(_8,_1)
-gA : gmem_ptr[16b](0x7f3f23c00000) o (_16,_16,7):(112,_1,_16)
-gB : gmem_ptr[16b](0x7f3f23c00e00) o (_8,_16,7):(112,_1,_16)
-rC : ptr[16b](0x7f3f4bfffcd0) o ((_2,_2),_1,_1):((_1,_2),_0,_0)
-tCgA : gmem_ptr[16b](0x7f3f23c00000) o ((_2,_2,_2),_1,_1,7):((_1,896,_8),_0,_0,_16)
-tCgB : gmem_ptr[16b](0x7f3f23c00e00) o ((_2,_2),_1,_1,7):((_1,_8),_0,_0,_16)
-tCmC : gmem_ptr[16b](0x7f3f23c01600) o ((_2,_2),_1,_1):((_1,_64),_0,_0)
-
-Next line fails with
-static_assert(is_static<Layout>::value, "Dynamic owning tensors not supported");
-*/
 #if 1
-  auto rA = thrmma.partition_fragment_A(gA(_, _, 0));
-  auto rB = thrmma.partition_fragment_B(gB(_, _, 0));
+  auto rA = thrmma.partition_fragment_A(tCgA(_, _, _, 0));
+  auto rB = thrmma.partition_fragment_B(tCgB(_, _, _, 0));
 #endif
-#if 0
+#if 1
   if (thread0()) {
     print("mA : "); print(mA); print("\n");
     print("mB : "); print(mB); print("\n");
@@ -99,15 +72,26 @@ static_assert(is_static<Layout>::value, "Dynamic owning tensors not supported");
     print("rA : "); print(rA); print("\n");
     print("rB : "); print(rB); print("\n");
     print("rC : "); print(rC); print("\n");
-    print("tCmA : "); print(tCmA); print("\n");
-    print("tCmB : "); print(tCmB); print("\n");
+    print("tCgA : "); print(tCgA); print("\n");
+    print("tCgB : "); print(tCgB); print("\n");
     print("tCmC : "); print(tCmC); print("\n");
   }
 #endif
-#if 1
+/*
+mA : gmem_ptr[16b](0x7ff473c00000) o (_16,112):(112,_1)
+mB : gmem_ptr[16b](0x7ff473c00e00) o (_8,112):(112,_1)
+mC : gmem_ptr[16b](0x7ff473c01600) o (_16,_8):(_8,_1)
+rA : ptr[16b](0x7ff49efffcc0) o ((_2,_2,_2),_1,_1,_1):((_0,_1,_0),_0,_0,_0)
+rB : ptr[16b](0x7ff49efffcd0) o ((_2,_2),_1,_1,_1):((_0,_0),_0,_0,_0)
+rC : ptr[16b](0x7ff49efffcb0) o ((_2,_2),_1,_1,_1):((_1,_2),_0,_0,_0)
+tCgA : gmem_ptr[16b](0x7ff473c00000) o ((_2,_2,_2),_1,_1,7):((_1,896,_8),_0,_0,_16)
+tCgB : gmem_ptr[16b](0x7ff473c00e00) o ((_2,_2),_1,_1,7):((_1,_8),_0,_0,_16)
+tCmC : gmem_ptr[16b](0x7ff473c01600) o ((_2,_2),_1,_1):((_1,_64),_0,_0)
+*/
+#if 0
   for (int p1 = 0; p1 < k1; ++p1) {
-    copy(tCgA, rA);
-    copy(tCgB, rB);
+    copy(tCgA(_, _, _, 0), rA);
+    copy(tCgB(_, _, _, 0), rB);
     gemm(my_mma, rA, rB, rC);
   }
   copy(rC, tCmC);
@@ -165,13 +149,13 @@ int main() {
 #if 1
   matrix_multiply_cpu(h_A.data(), h_B.data(), h_C.data(), m, n, k);
 #endif
-#if 1
+#if 0
   print("h_A : "); printMatrix(h_A.data(), m, k); print("\n\n");
   print("h_B : "); printMatrix(h_B.data(), k, n); print("\n\n");
   print("h_C : "); printMatrix(h_C.data(), m, n); print("\n\n");
   print("cute_result : "); printMatrix(cute_result.data(), m, n); print("\n\n");
 #endif
-# if 1
+# if 0
   assert(areMatricesEqual(cute_result.data(), h_C.data(), m, n));
   std::cout << "Success!" << std::endl;
 #endif
